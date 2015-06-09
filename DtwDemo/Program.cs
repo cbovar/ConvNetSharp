4a619df4735107f4e568680943393939e0fba751
cbovar <cedric.bovar@gmail.com>
2015-06-09 08:01:56 +0800

    Dtw layer

---
+++ DtwDemo/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using ConvNetSharp;
+
+namespace DtwDemo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //double[] s = { 0.0, 1.0, 2.0, 3.0, 0.0, 1.0, 2.0, 3.0, 0.0, 0.0 };
+            //double[] t1 = { 0.0, 1.0, 2.0 };
+            //double[] t2 = { 0.0, 0.0, 1.0, 2.0 };
+            //double[] t3 = { 0.0, 0.0, 0.0, 1.0, 2.0 };
+            //double[] t4 = { 0.0, 0.0, 0.0, 0.0, 1.0, 2.0 };
+            //double[] t5 = { 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 2.0 };
+
+
+            //var d1 = DynamicTimeWarpLayer.Dtw(s, t1, 2);
+            //var d2 = DynamicTimeWarpLayer.Dtw(s, t2, 2);
+            //var d3 = DynamicTimeWarpLayer.Dtw(s, t3, 2);
+            //var d4 = DynamicTimeWarpLayer.Dtw(s, t4, 2);
+            //var d5 = DynamicTimeWarpLayer.Dtw(s, t5, 2);
+
+            List<double> data = new List<double>();
+            using (var sr = new StreamReader(@"C:\pro\temp\test.csv"))
+            {
+                sr.ReadLine();//header
+
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(',')[1];
+                    data.Add(double.Parse(line));
+                }
+            }
+
+            var net = new Net();
+            net.AddLayer(new InputLayer(10, 1, 1));
+            net.AddLayer(new DynamicTimeWarpLayer(3, 2));
+            net.AddLayer(new RegressionLayer(1));
+
+
+            //var input1 = new Volume(10, 1, 1, 0);
+            //input1.Set(0, 0, 0, 0.0);
+            //input1.Set(1, 0, 0, 1.0);
+            //input1.Set(2, 0, 0, 2.0);
+            //input1.Set(3, 0, 0, 1.0);
+            //input1.Set(2, 0, 0, 0.0);
+
+            //var input2 = new Volume(10, 1, 1, 0);
+            //input2.Set(0, 0, 0, 2.0);
+            //input2.Set(1, 0, 0, 1.0);
+            //input2.Set(2, 0, 0, 0.0);
+            //input2.Set(3, 0, 0, 1.0);
+            //input2.Set(2, 0, 0, 2.0);
+
+
+            var trainer = new Trainer(net) { LearningRate = 0.01, Momentum = 0.0, BatchSize = 10, L2Decay = 0.001 };
+
+            //double[] input = new double[5];
+
+            do
+            {
+                for (int i = 0; i < data.Count-6; i++)
+                {
+                    var vol = new Volume(5, 1, 1, 0);
+
+                    for (int j = 0; j < 5; j++)
+                    {
+                        vol.Set(j, 0, 0, data[i + j]);
+                        //input[j] = data[i + j];
+                    }
+
+                    var res1 = net.Forward(vol);
+                    trainer.Train(vol, data[i + 5]);
+                }
+
+
+
+                
+               
+
+           //     var res1 = net.Forward(input1);
+                //var res2 = net.Forward(input2);
+
+                Console.WriteLine(trainer.CostLoss);
+
+            } while (true);
+        }
+    }
+}
---
+++ src/ConvNetSharp/DynamicTimewarpLayer.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace ConvNetSharp
+{
+    public enum Transformation
+    {
+        Convolution,
+        DynamicTimeWarp
+    }
+
+
+    public class DynamicTimeWarpLayer : LayerBase
+    {
+        private Volume biases;
+
+        public DynamicTimeWarpLayer(int width, int filterCount)
+        {
+            this.GroupSize = 2;
+            this.L1DecayMul = 0.0;
+            this.L2DecayMul = 1.0;
+            this.Stride = 1;
+            this.Pad = 0;
+
+            this.FilterCount = filterCount;
+            this.Width = width;
+            this.Height = 1;
+        }
+
+        public List<Volume> Filters { get; private set; }
+
+        public int FilterCount { get; private set; }
+
+        public double L1DecayMul { get; set; }
+
+        public double L2DecayMul { get; set; }
+
+        public int Stride { get; set; }
+
+        public int Pad { get; set; }
+
+        public double BiasPref { get; set; }
+
+        public Activation Activation { get; set; }
+
+        public int GroupSize { get; private set; }
+
+        public Transformation Transformation { get; set; }
+
+        public override Volume Forward(Volume volume, bool isTraining = false)
+        {
+            this.InputActivation = volume;
+            var outputActivation = new Volume(this.OutputWidth, 1, this.OutputDepth, 0.0);
+
+            for (var depth = 0; depth < this.OutputDepth; depth++)
+            {
+                var filter = this.Filters[depth];
+                var x = -this.Pad;
+                for (var ax = 0; ax < this.OutputWidth; x += this.Stride, ax++)
+                {
+                    var a = DTW(volume, filter, x);
+                    //a += this.biases.Weights[depth];
+                    outputActivation.Set(ax, 0, depth, a);
+                }
+            }
+
+            this.OutputActivation = outputActivation;
+            return this.OutputActivation;
+        }
+
+        private static double Convolve(Volume volume, Volume filter, int x)
+        {
+            // convolve centered at this particular location
+            var a = 0.0;
+            for (var fx = 0; fx < filter.Width; fx++)
+            {
+                var ox = x + fx;
+                if (ox >= 0 && ox < volume.Width)
+                {
+                    a += filter.Weights[fx] * volume.Weights[ox];
+                }
+            }
+            return a;
+        }
+
+        private static double Simple(Volume volume, Volume filter, int x)
+        {
+            // convolve centered at this particular location
+            var a = 0.0;
+            for (var fx = 0; fx < filter.Width; fx++)
+            {
+                var ox = x + fx;
+                if (ox >= 0 && ox < volume.Width)
+                {
+                    a += Math.Abs(filter.Weights[fx] - volume.Weights[ox]);
+                }
+            }
+            return a;
+        }
+
+        private static double DTW(Volume volume, Volume filter, int x)
+        {
+            // convolve centered at this particular location
+            var a = 0.0;
+
+            var t = new double[filter.Width];
+            for (int fx = 0; fx < filter.Width; fx++)
+            {
+                var ox = x + fx;
+                if (ox >= 0 && ox < volume.Width)
+                {
+                    t[fx] = volume.Weights[ox];
+                }
+            }
+
+            a = Dtw(filter.Weights, t, 3);
+
+            return a;
+        }
+
+        public static double Dtw(double[] s, double[] t, int w)
+        {
+            int n = s.Length;
+            int m = t.Length;
+            w = Math.Max(w, Math.Abs(n - m));
+            var dtw = new double[n, m];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    dtw[i, j] = double.MaxValue;
+                }
+            }
+            dtw[0, 0] = 0;
+
+            for (int i = 1; i < n; i++)
+            {
+                var from = Math.Max(1, i - w);
+                var to = Math.Min(m - 1, i + w);
+
+                for (int j = from; j <= to; j++)
+                {
+                    var cost = Math.Sqrt((s[i] - t[j]) * (s[i] - t[j])); // distance
+                    dtw[i, j] = cost + Math.Min(Math.Min(dtw[i - 1, j], dtw[i, j - 1]), dtw[i - 1, j - 1]);
+                }
+            }
+
+            return dtw[n - 1, m - 1];
+        }
+
+        public override void Backward()
+        {
+            var volume = this.InputActivation;
+            volume.WeightGradients = new double[volume.Weights.Length]; // zero out gradient wrt bottom data, we're about to fill it
+
+            for (var depth = 0; depth < this.OutputDepth; depth++)
+            {
+                var filter = this.Filters[depth];
+
+                // xyStride
+                var x = -this.Pad;
+                for (var ax = 0; ax < this.OutputWidth; x += this.Stride, ax++)
+                {
+                    // convolve centered at this particular location
+                    var chainGradient = this.OutputActivation.GetGradient(ax, 0, depth);
+                    // gradient from above, from chain rule
+                    for (var fx = 0; fx < filter.Width; fx++)
+                    {
+                        var ox = x + fx;
+                        if (ox >= 0 && ox < volume.Width)
+                        {
+                            // avoid function call overhead (x2) for efficiency, compromise modularity :(
+                            filter.WeightGradients[fx] += chainGradient;
+                            volume.WeightGradients[ox] += -chainGradient; ;
+                            //filter.WeightGradients[fx] += (volume.Weights[ox] - filter.Weights[fx]) / Math.Abs(filter.Weights[fx] - volume.Weights[ox]) * chainGradient;
+                            //volume.WeightGradients[ox] += -(volume.Weights[ox] - filter.Weights[fx]) / Math.Abs(volume.Weights[ox] - filter.Weights[fx]) * chainGradient; ;
+
+                            //filter.WeightGradients[fx] += volume.Weights[ox] * chainGradient;
+                            //volume.WeightGradients[ox] += filter.Weights[fx] * chainGradient;
+                        }
+                    }
+
+                    this.biases.WeightGradients[depth] += chainGradient;
+                }
+            }
+        }
+
+        public override void Init(int inputWidth, int inputHeight, int inputDepth)
+        {
+            if (inputHeight != 1)
+            {
+                throw new ArgumentException("inputHeight must be 1", "inputHeight");
+            }
+
+            if (inputDepth != 1)
+            {
+                throw new ArgumentException("inputDepth must be 1", "inputDepth");
+            }
+
+            base.Init(inputWidth, inputHeight, inputDepth);
+
+            // required
+            this.OutputDepth = this.FilterCount;
+
+            // computed
+            // note we are doing floor, so if the strided convolution of the filter doesnt fit into the input
+            // volume exactly, the output volume will be trimmed and not contain the (incomplete) computed
+            // final application.
+            this.OutputWidth = (int)Math.Floor((this.InputWidth + this.Pad * 2 - this.Width) / (double)this.Stride + 1);
+            this.OutputHeight = 1;
+
+            // initializations
+            var bias = this.BiasPref;
+            this.Filters = new List<Volume>();
+
+            for (var i = 0; i < this.OutputDepth; i++)
+            {
+                this.Filters.Add(new Volume(this.Width, 1, 1));
+            }
+
+            this.biases = new Volume(1, 1, this.OutputDepth, bias);
+        }
+
+        public override List<ParametersAndGradients> GetParametersAndGradients()
+        {
+            var response = new List<ParametersAndGradients>();
+            for (var i = 0; i < this.OutputDepth; i++)
+            {
+                response.Add(new ParametersAndGradients
+                {
+                    Parameters = this.Filters[i].Weights,
+                    Gradients = this.Filters[i].WeightGradients,
+                    L2DecayMul = this.L2DecayMul,
+                    L1DecayMul = this.L1DecayMul
+                });
+            }
+
+            response.Add(new ParametersAndGradients
+            {
+                Parameters = this.biases.Weights,
+                Gradients = this.biases.WeightGradients,
+                L1DecayMul = 0.0,
+                L2DecayMul = 0.0
+            });
+
+            return response;
+        }
+    }
+}
