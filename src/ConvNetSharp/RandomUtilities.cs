9cdbbf9c8f4fc525826e01a5c3657ff27413781d
cbovar <cedric.bovar@gmail.com>
2015-06-17 23:40:08 +0800

    Fix convergence speed issue

---
+++ src/ConvNetSharp/RandomUtilities.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConvNetSharp
+{
+    public static class RandomUtilities
+    {
+        public static Random Random = new Random((int) DateTime.Now.Ticks);
+        private static double val;
+        private static bool returnVal;
+
+        public static double GaussianRandom()
+        {
+            if (returnVal)
+            {
+                returnVal = false;
+                return val;
+            }
+
+            var u = 2*Random.NextDouble() - 1;
+            var v = 2*Random.NextDouble() - 1;
+            var r = u*u + v*v;
+
+            if (r == 0 || r > 1)
+            {
+                return GaussianRandom();
+            }
+
+            var c = Math.Sqrt(-2*Math.Log(r)/r);
+            val = v*c; // cache this
+            returnVal = true;
+
+            return u*c;
+        }
+
+        public static double Randn(double mu, double std)
+        {
+            return mu + GaussianRandom()*std;
+        }
+    }
+}
