ab35fd76eba20f57221d73a21e668f205cac361f
cbovar <cedric.bovar@gmail.com>
2015-07-04 13:21:24 +0800

    Unit tests
    Relu layer gradient check

---
+++ src/ConvNetSharp.Tests/ReluLayerTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace ConvNetSharp.Tests
+{
+    [TestFixture]
+    public class ReluLayerTests
+    {
+        [Test]
+        public void GradientWrtInputCheck()
+        {
+            const int inputWidth = 20;
+            const int inputHeight = 20;
+            const int inputDepth = 2;
+
+            // Create layer
+            var layer = new ReluLayer();
+
+            GradientCheckTools.GradientCheck(layer, inputWidth, inputHeight, inputDepth, 1e-6);
+        }
+    }
+}
