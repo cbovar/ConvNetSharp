a9bc5be4cbe75893ba1248e79eaa012e2274f284
cbovar <cedric.bovar@gmail.com>
2015-07-28 23:48:33 +0800

    Make convlayer tests more complete

--- src/ConvNetSharp.Tests/ConvLayerTests.cs
+++ src/ConvNetSharp.Tests/ConvLayerTests.cs
@@ -8,16 +8,16 @@ namespace ConvNetSharp.Tests
         [Test]
         public void GradientWrtInputCheck()
         {
-            const int inputWidth = 10;
-            const int inputHeight = 10;
+            const int inputWidth = 30;
+            const int inputHeight = 30;
             const int inputDepth = 2;
 
             // Create layer
             const int filterWidth = 3;
             const int filterHeight = 3;
-            const int filterCount = 2;
+            const int filterCount = 5;
 
-            var layer = new ConvLayer(filterWidth, filterHeight, filterCount);
+            var layer = new ConvLayer(filterWidth, filterHeight, filterCount) { Stride = 2};
 
             GradientCheckTools.GradientCheck(layer, inputWidth, inputHeight, inputDepth);
         }
@@ -34,7 +34,7 @@ namespace ConvNetSharp.Tests
             const int filterHeight = 3;
             const int filterCount = 2;
 
-            var layer = new ConvLayer(filterWidth, filterHeight, filterCount);
+            var layer = new ConvLayer(filterWidth, filterHeight, filterCount) { Stride = 2 };
 
             GradientCheckTools.GradienWrtParameterstCheck(inputWidth, inputHeight, inputDepth, layer);
         }
