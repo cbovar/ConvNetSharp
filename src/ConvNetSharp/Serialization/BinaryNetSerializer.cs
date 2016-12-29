585e64da6109c65f4f418dfb2d968224b425b9a1
cbovar <cedric.bovar@gmail.com>
2016-12-29 19:55:48 +0800

    remove commented lines

--- src/ConvNetSharp/Serialization/BinaryNetSerializer.cs
+++ src/ConvNetSharp/Serialization/BinaryNetSerializer.cs
@@ -6,30 +6,6 @@ namespace ConvNetSharp.Serialization
 {
     public class BinaryNetSerializer : INetSerializer
     {
-        //public void Save(string filename)
-        //{
-        //    using (var fs = new FileStream(filename, FileMode.Create))
-        //    {
-        //        IFormatter formatter = new BinaryFormatter();
-        //        formatter.Serialize(fs, this);
-        //    }
-        //}
-
-        //public static Net Load(string filename)
-        //{
-        //    Net result = null;
-        //    if (File.Exists(filename))
-        //    {
-        //        using (var fs = new FileStream(filename, FileMode.Open))
-        //        {
-        //            IFormatter formatter = new BinaryFormatter();
-        //            result = formatter.Deserialize(fs) as Net;
-        //        }
-        //    }
-
-        //    return result;
-        //}
-
         public Net Load(Stream stream)
         {
             IFormatter formatter = new BinaryFormatter();
