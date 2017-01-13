c969af3fb724ecb14975d69337a81437676abb2c
cbovar <cedric.bovar@gmail.com>
2017-01-13 20:01:11 +0800

    improve serialization tests

--- src/ConvNetSharp.Tests/SerializationTests.cs
+++ src/ConvNetSharp.Tests/SerializationTests.cs
@@ -14,7 +14,10 @@ namespace ConvNetSharp.Tests
         {
             var net = new Net();
             net.AddLayer(new InputLayer(5, 5, 3));
-            net.AddLayer(new FullyConnLayer(3));
+            var conv = new ConvLayer(2, 2, 16);
+            net.AddLayer(conv);
+            var fullycon = new FullyConnLayer(3);
+            net.AddLayer(fullycon);
             net.AddLayer(new SoftmaxLayer(3));
 
             // Serialize to json
@@ -23,32 +26,63 @@ namespace ConvNetSharp.Tests
             // Deserialize from json
             Net deserialized = SerializationExtensions.FromJSON(json);
 
+            // Make sure deserialized is identical to serialized
             Assert.IsNotNull(deserialized.Layers);
             Assert.AreEqual(net.Layers.Count, deserialized.Layers.Count);
             Assert.IsTrue(net.Layers[0] is InputLayer);
-            Assert.IsTrue(net.Layers[1] is FullyConnLayer);
-            Assert.IsTrue(net.Layers[2] is SoftmaxLayer);
+
+            var deserializedConv = net.Layers[1] as ConvLayer;
+            Assert.NotNull(deserializedConv);
+            Assert.NotNull(deserializedConv.Filters);
+            Assert.AreEqual(16, deserializedConv.Filters.Count);
+            for (int i = 0; i < deserializedConv.Filters.Count; i++)
+            {
+                for (int k = 0; k < deserializedConv.Filters[i].Length; k++)
+                {
+                    Assert.AreEqual(conv.Filters[i].Get(k), deserializedConv.Filters[i].Get(k));
+                    Assert.AreEqual(conv.Filters[i].GetGradient(k), deserializedConv.Filters[i].GetGradient(k));
+                }
+            }
+
+            var deserializedFullyCon = net.Layers[2] as FullyConnLayer;
+            Assert.NotNull(deserializedFullyCon);
+            Assert.NotNull(deserializedFullyCon.Filters);
+            Assert.AreEqual(3, deserializedFullyCon.Filters.Count);
+            for (int i = 0; i < deserializedFullyCon.Filters.Count; i++)
+            {
+                for (int k = 0; k < deserializedFullyCon.Filters[i].Length; k++)
+                {
+                    Assert.AreEqual(fullycon.Filters[i].Get(k), deserializedFullyCon.Filters[i].Get(k));
+                    Assert.AreEqual(fullycon.Filters[i].GetGradient(k), deserializedFullyCon.Filters[i].GetGradient(k));
+                }
+            }
+
+            Assert.IsTrue(net.Layers[3] is SoftmaxLayer);
+            Assert.AreEqual(3, ((SoftmaxLayer)net.Layers[3]).ClassCount);
         }
 
         [Test]
-        public void FluentJsonNetSerializerTest()
+        public void FluentBinaryNetSerializerTest()
         {
             var net = FluentNet.Create(5, 5, 3)
+                .Conv(2, 2, 16)
                 .FullyConn(3)
                 .Softmax(3)
                 .Build();
 
-            // Serialize to json
+            // Serialize (binary)
             using (var ms = new MemoryStream())
             {
                 net.SaveBinary(ms);
                 ms.Position = 0;
 
-                // Deserialize from json
+                // Deserialize (binary)
                 FluentNet deserialized = SerializationExtensions.LoadBinary(ms) as FluentNet;
 
                 Assert.IsNotNull(deserialized);
                 Assert.AreEqual(net.InputLayers.Count, deserialized.InputLayers.Count);
+
+                // TODO: improve test
             }
         }
 
@@ -57,23 +91,54 @@ namespace ConvNetSharp.Tests
         {
             var net = new Net();
             net.AddLayer(new InputLayer(5, 5, 3));
-            net.AddLayer(new FullyConnLayer(3));
+            var conv = new ConvLayer(2, 2, 16);
+            net.AddLayer(conv);
+            var fullycon = new FullyConnLayer(3);
+            net.AddLayer(fullycon);
             net.AddLayer(new SoftmaxLayer(3));
 
-            // Serialize to json
+            // Serialize (binary)
             using (var ms = new MemoryStream())
             {
                 net.SaveBinary(ms);
                 ms.Position = 0;
 
-                // Deserialize from json
+                // Deserialize (binary)
                 Net deserialized = SerializationExtensions.LoadBinary(ms) as Net;
 
+                // Make sure deserialized is identical to serialized
                 Assert.IsNotNull(deserialized.Layers);
                 Assert.AreEqual(net.Layers.Count, deserialized.Layers.Count);
                 Assert.IsTrue(net.Layers[0] is InputLayer);
-                Assert.IsTrue(net.Layers[1] is FullyConnLayer);
-                Assert.IsTrue(net.Layers[2] is SoftmaxLayer);
+
+                var deserializedConv = net.Layers[1] as ConvLayer;
+                Assert.NotNull(deserializedConv);
+                Assert.NotNull(deserializedConv.Filters);
+                Assert.AreEqual(16, deserializedConv.Filters.Count);
+                for (int i = 0; i < deserializedConv.Filters.Count; i++)
+                {
+                    for (int k = 0; k < deserializedConv.Filters[i].Length; k++)
+                    {
+                        Assert.AreEqual(conv.Filters[i].Get(k), deserializedConv.Filters[i].Get(k));
+                        Assert.AreEqual(conv.Filters[i].GetGradient(k), deserializedConv.Filters[i].GetGradient(k));
+                    }
+                }
+
+                var deserializedFullyCon = net.Layers[2] as FullyConnLayer;
+                Assert.NotNull(deserializedFullyCon);
+                Assert.NotNull(deserializedFullyCon.Filters);
+                Assert.AreEqual(3, deserializedFullyCon.Filters.Count);
+                for (int i = 0; i < deserializedFullyCon.Filters.Count; i++)
+                {
+                    for (int k = 0; k < deserializedFullyCon.Filters[i].Length; k++)
+                    {
+                        Assert.AreEqual(fullycon.Filters[i].Get(k), deserializedFullyCon.Filters[i].Get(k));
+                        Assert.AreEqual(fullycon.Filters[i].GetGradient(k), deserializedFullyCon.Filters[i].GetGradient(k));
+                    }
+                }
+
+                Assert.IsTrue(net.Layers[3] is SoftmaxLayer);
+                Assert.AreEqual(3, ((SoftmaxLayer)net.Layers[3]).ClassCount);
             }
         }
     }
