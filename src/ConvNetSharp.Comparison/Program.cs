462d577837a587c0b7d3107f72fb7d704a7479b0
Augustin Juricic <ajuricic@neogov.net>
2017-04-02 12:57:22 +0200

    Refactor.

--- src/ConvNetSharp.Comparison/Program.cs
+++ src/ConvNetSharp.Comparison/Program.cs
@@ -14,13 +14,13 @@ namespace ConvNetSharp.Comparison
     {
         public static void Main(string[] args)
         {
-            //var model = new XorTrainingModel();
-            var model = new CircledRegionTrainingModel();
+            var model = new XorTrainingModel();
+            //var model = new CircledRegionTrainingModel();
 
             const int BATCH_SIZE = 10;
             const double LEARN_RATE = 0.01;
             const double MOMENTUM = 0.05;
-            
+
             var aNet = CreateOldNet(model.NmInputs, model.NmOutputs);
             var bNet = CreateNewNet(model.NmInputs, model.NmOutputs);
             var builder = BVolume.BuilderInstance<double>.Volume;
@@ -46,42 +46,51 @@ namespace ConvNetSharp.Comparison
             int xIn = 0, xOut = 0;
             var bIn = builder.SameAs(new BVolume.Shape(1, 1, model.NmInputs, BATCH_SIZE));
             var bOut = builder.SameAs(new BVolume.Shape(1, 1, model.NmOutputs, BATCH_SIZE));
-            for (var epoch = 0; epoch < 50; epoch++)
+
+            int epoch = 0;
+            int i = 0;
+            while (epoch < 50)
             {
-                for (var i = 0; i < model.Inputs.Count; i++)
+                var input = model.Inputs[i];
+                var output = model.Outputs[i];
+
+                //convert input and output to what trainer expects
+                var aIn = new A.Volume(1, 1, model.NmInputs);
+                for (var x = 0; x < input.Length; x++)
+                    aIn.Set(x, input[x]);
+                var aOut = Array.IndexOf(output, 1.0);
+
+                //trainer A takes care of batching inside
+                //expects a class number, not softmax array
+                aTrainer.Train(aIn, aOut);
+
+                //pool input and output into batch for trainer B
+                foreach (var v in input)
+                    bIn.Set(xIn++, v);
+                foreach (var v in output)
+                    bOut.Set(xOut++, v);
+
+                //when batch for B full, execute training
+                if (xIn == bIn.Shape.TotalLength)
                 {
-                    var input = model.Inputs[i];
-                    var output = model.Outputs[i];
-
-                    //convert input and output to what trainer expects
-                    var aIn = new A.Volume(1, 1, model.NmInputs);
-                    for (var x = 0; x < input.Length; x++)
-                        aIn.Set(x, input[x]);
-                    var aOut = Array.IndexOf(output, 1.0);
-
-                    //trainer A takes care of batching inside
-                    //expects a class number, not softmax array
-                    aTrainer.Train(aIn, aOut);
-
-                    //pool input and output into batch for trainer B
-                    foreach (var v in input)
-                        bIn.Set(xIn++, v);
-                    foreach (var v in output)
-                        bOut.Set(xOut++, v);
-
-                    //when batch for B full, execute training
-                    if (xIn == bIn.Shape.TotalLength)
-                    {
-                        Debug.Assert(xOut == bOut.Shape.TotalLength);
-                        xIn = 0;
-                        xOut = 0;
-                        bTrainer.Train(bIn, bOut);
-                    }
+                    Debug.Assert(xOut == bOut.Shape.TotalLength);
+                    xIn = 0;
+                    xOut = 0;
+                    bTrainer.Train(bIn, bOut);
                 }
 
-                //write A vs B losses
-                Console.WriteLine(
-                    $"{epoch:000}       A == {aTrainer.Loss:0.000} vs B == {bTrainer.Loss:0.000} ----> DIFF {aTrainer.Loss - bTrainer.Loss: 0.000}");
+                i++;
+                if (i >= model.Inputs.Count)
+                {
+                    var diff = Math.Abs(aTrainer.Loss - bTrainer.Loss);
+
+                    //write A vs B losses
+                    Console.WriteLine(
+                        $"{epoch:000}       A == {aTrainer.Loss:0.000} vs B == {bTrainer.Loss:0.000} " +
+                        $"----> DIFF {diff: 0.000}");
+                    epoch++;
+                    i = 0;
+                }
             }
         }
 
