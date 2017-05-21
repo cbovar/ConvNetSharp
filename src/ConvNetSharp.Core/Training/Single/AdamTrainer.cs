a0f13f70e7225e1a04696ce1a21ff94673e4f1d6
cbovar <cedric.bovar@gmail.com>
2017-05-21 20:44:22 +0800

    Adam

---
+++ src/ConvNetSharp.Core/Training/Single/AdamTrainer.cs
@@ -0,0 +1,9 @@
+﻿namespace ConvNetSharp.Core.Training.Single
+{
+    public class AdamTrainer : AdamTrainer<float>
+    {
+        public AdamTrainer(INet<float> net) : base(net)
+        {
+        }
+    }
+}
