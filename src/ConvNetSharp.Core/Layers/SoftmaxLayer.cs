73c0b4b2d3afca25e8b61cadeff1f753cea17ce1
cbovar <cedric.bovar@gmail.com>
2017-06-01 20:06:23 +0800

    cross entropy loss derivative rather than softmax derivative

--- src/ConvNetSharp.Core/Layers/SoftmaxLayer.cs
+++ src/ConvNetSharp.Core/Layers/SoftmaxLayer.cs
@@ -18,9 +18,16 @@ namespace ConvNetSharp.Core.Layers
 
         public int ClassCount { get; set; }
 
+        /// <summary>
+        /// This computes the cross entropy loss and its gradient (not the softmax gradient)
+        /// </summary>
+        /// <param name="y"></param>
+        /// <param name="loss"></param>
         public override void Backward(Volume<T> y, out T loss)
         {
-            this.OutputActivation.DoSoftMaxGradient(this.OutputActivation - y, this.InputActivationGradients);
+            // input gradient = pi - yi
+            y.DoSubtractFrom(this.OutputActivation, this.InputActivationGradients);
+            //this.OutputActivation.Storage.Map((o, yi) => Ops<T>.Add(o, Ops<T>.Negate(yi)), y.Storage, this.InputActivationGradients.Storage);
 
             //loss is the class negative log likelihood
             loss = Ops<T>.Zero;
