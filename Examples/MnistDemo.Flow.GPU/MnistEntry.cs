9e78bc79218354d3ac9dfd1ec83c68827e66a13a
cbovar <cedric.bovar@gmail.com>
2017-06-22 23:37:13 +0800

    Mnist Flow GPU demo

---
+++ Examples/MnistDemo.Flow.GPU/MnistEntry.cs
@@ -0,0 +1,14 @@
+namespace MnistDemo.GPU
+{
+    public class MnistEntry
+    {
+        public byte[] Image { get; set; }
+
+        public int Label { get; set; }
+
+        public override string ToString()
+        {
+            return "Label: " + this.Label;
+        }
+    }
+}
