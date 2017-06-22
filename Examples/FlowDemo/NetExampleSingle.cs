6702cb5963e27da77db28bca071dc982ff6ae1d4
cbovar <cedric.bovar@gmail.com>
2017-06-22 23:43:31 +0800

    clean up

--- Examples/FlowDemo/NetExampleSingle.cs
+++ Examples/FlowDemo/NetExampleSingle.cs
@@ -6,6 +6,7 @@ using ConvNetSharp.Flow.Layers;
 using ConvNetSharp.Flow.Training;
 using ConvNetSharp.Utils.GraphVisualizer;
 using ConvNetSharp.Volume;
+using ConvNetSharp.Volume.Double;
 
 namespace FlowDemo
 {
@@ -27,31 +28,31 @@ namespace FlowDemo
             // Data
             var data = new List<double[]>();
             var labels = new List<int>();
-            data.Add(new[] {-0.4326, 1.1909});
+            data.Add(new[] { -0.4326, 1.1909 });
             labels.Add(1);
-            data.Add(new[] {3.0, 4.0});
+            data.Add(new[] { 3.0, 4.0 });
             labels.Add(1);
-            data.Add(new[] {0.1253, -0.0376});
+            data.Add(new[] { 0.1253, -0.0376 });
             labels.Add(1);
-            data.Add(new[] {0.2877, 0.3273});
+            data.Add(new[] { 0.2877, 0.3273 });
             labels.Add(1);
-            data.Add(new[] {-1.1465, 0.1746});
+            data.Add(new[] { -1.1465, 0.1746 });
             labels.Add(1);
-            data.Add(new[] {1.8133, 1.0139});
+            data.Add(new[] { 1.8133, 1.0139 });
             labels.Add(0);
-            data.Add(new[] {2.7258, 1.0668});
+            data.Add(new[] { 2.7258, 1.0668 });
             labels.Add(0);
-            data.Add(new[] {1.4117, 0.5593});
+            data.Add(new[] { 1.4117, 0.5593 });
             labels.Add(0);
-            data.Add(new[] {4.1832, 0.3044});
+            data.Add(new[] { 4.1832, 0.3044 });
             labels.Add(0);
-            data.Add(new[] {1.8636, 0.1677});
+            data.Add(new[] { 1.8636, 0.1677 });
             labels.Add(0);
-            data.Add(new[] {0.5, 3.2});
+            data.Add(new[] { 0.5, 3.2 });
             labels.Add(1);
-            data.Add(new[] {0.8, 3.2});
+            data.Add(new[] { 0.8, 3.2 });
             labels.Add(1);
-            data.Add(new[] {1.0, -2.2});
+            data.Add(new[] { 1.0, -2.2 });
             labels.Add(1);
             var n = labels.Count;
 
@@ -65,14 +66,14 @@ namespace FlowDemo
             // Display graph
             var vm = new ViewModel<double>(net.Cost);
             var app = new Application();
-            app.Run(new GraphControl {DataContext = vm});
+            app.Run(new GraphControl { DataContext = vm });
         }
 
         private static void Classify2DUpdate(int n, List<double[]> data, TrainerBase<double> trainer, List<int> labels)
         {
             var avloss = 0.0;
-            var netx = BuilderInstance<double>.Volume.SameAs(new Shape(1, 1, 2, n));
-            var hotLabels = BuilderInstance<double>.Volume.SameAs(new Shape(1, 1, 2, n));
+            var netx = BuilderInstance.Volume.SameAs(new Shape(1, 1, 2, n));
+            var hotLabels = BuilderInstance.Volume.SameAs(new Shape(1, 1, 2, n));
 
             for (var ix = 0; ix < n; ix++)
             {
