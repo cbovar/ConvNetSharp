b83f8d899de527bdc1af074a4a051b75bab6ca8c
cbovar <cedric.bovar@gmail.com>
2017-07-02 23:05:30 +0800

    Add missing BiasPref to FullyConnLayer

--- src/ConvNetSharp.Flow/Layers/FullyConnLayer.cs
+++ src/ConvNetSharp.Flow/Layers/FullyConnLayer.cs
@@ -7,12 +7,27 @@ namespace ConvNetSharp.Flow.Layers
     public class FullyConnLayer<T> : LayerBase<T> where T : struct, IEquatable<T>, IFormattable
     {
         private readonly int _neuronCount;
-
+        private bool _initialized;
+        private T _biasPref;
         private Variable<T> _bias;
 
         public FullyConnLayer(int neuronCount)
         {
             this._neuronCount = neuronCount;
+        }
+
+        public T BiasPref
+        {
+            get { return this._biasPref; }
+            set
+            {
+                this._biasPref = value;
+
+                if (this._initialized)
+                {
+                    this._bias.Result = new T[this._neuronCount].Populate(this.BiasPref);
+                }
+            }
         }
 
         public override void AcceptParent(LayerBase<T> parent)
@@ -26,6 +41,8 @@ namespace ConvNetSharp.Flow.Layers
                 this._bias = cns.Variable(BuilderInstance<T>.Volume.SameAs(new Shape(1, 1, this._neuronCount, 1)), "Bias");
                 this.Op = cns.Conv(cns.Reshape(parent.Op, new Shape(1, 1, -1, Shape.Keep)), 1, 1, this._neuronCount) + this._bias;
             }
+
+            this._initialized = true;
         }
     }
 }
