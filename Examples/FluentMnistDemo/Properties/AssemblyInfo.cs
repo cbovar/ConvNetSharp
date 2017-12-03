9ea75e98dc635543e5f87b6daf08b93f71d0cb32
cbovar <cedric.bovar@gmail.com>
2017-12-03 11:04:51 +0800

    revert resharper replace

--- Examples/FluentMnistDemo/Properties/AssemblyInfo.cs
+++ Examples/FluentMnistDemo/Properties/AssemblyInfo.cs
@@ -26,10 +26,10 @@ using System.Runtime.InteropServices;
 //
 //      Major Version
 //      Minor Version
-//      From Number
+//      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the From and Revision Numbers
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
