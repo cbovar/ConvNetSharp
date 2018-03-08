02a2680aecb91a6fd2615545f5f1dd3c09076074
cbovar <cedric.bovar@gmail.com>
2018-03-08 08:01:40 +0800

    Add missing dropout serialization

---
+++ src/ConvNetSharp.Core/Layers/Double/DropoutLayer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConvNetSharp.Core.Layers.Double
+{
+    public class DropoutLayer : DropoutLayer<double>
+    {
+        public DropoutLayer()
+        {
+        }
+
+        public DropoutLayer(Dictionary<string, object> data) : base(data)
+        {
+            this.DropProbability = Convert.ToDouble(data["DropProbability"]);
+        }
+    }
+}
