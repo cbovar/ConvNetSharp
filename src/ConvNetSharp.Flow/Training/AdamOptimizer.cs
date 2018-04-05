4b5f6e3352074b06c2f1656319549ca531bd67c2
cbovar <cedric.bovar@gmail.com>
2018-04-05 21:52:41 +0800

    move comment

--- src/ConvNetSharp.Flow/Training/AdamOptimizer.cs
+++ src/ConvNetSharp.Flow/Training/AdamOptimizer.cs
@@ -76,7 +76,7 @@ namespace ConvNetSharp.Flow.Training
                     var learningRate = this._cns.PlaceHolder("lr"); // learning rate
 
                     var m_t = this._cns.Assign(m, beta1 * m + (one - beta1) * grad);  // m_t <- beta1 * m_{t-1} + (1 - beta1) * g
-                                                                                      //  m_t.Evaluated += (sender, args) => { Console.WriteLine($"m[{variable}]={ ((Op<T>)sender).Result.Get(0)}"); };
+                    //m_t.Evaluated += (sender, args) => { Console.WriteLine($"m[{variable}]={ ((Op<T>)sender).Result.Get(0)}"); };
 
                     var v_t = this._cns.Assign(v, beta2 * v + (one - beta2) * grad * grad);  // beta2 * v_{t-1} + (1 - beta2) * g * g
                     //v_t.Evaluated += (sender, args) => { Console.WriteLine($"v[{variable}]={ ((Op<T>)sender).Result.Get(0)}"); };
