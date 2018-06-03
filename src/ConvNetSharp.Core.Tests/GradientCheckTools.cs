71f8629806288214c641c7e0f5164421a32e48f6
cbovar <cedric.bovar@gmail.com>
2018-06-03 10:55:40 +0800

    Get rid of volume operatpr

--- src/ConvNetSharp.Core.Tests/GradientCheckTools.cs
+++ src/ConvNetSharp.Core.Tests/GradientCheckTools.cs
@@ -14,7 +14,8 @@ namespace ConvNetSharp.Core.Tests
             layer.Init(inputWidth, inputHeight, inputDepth);
 
             // Forward pass
-            var input = BuilderInstance<double>.Volume.Random(new Shape(inputWidth, inputHeight, inputDepth, bactchSize), 0.0, Math.Sqrt(2.0 / (inputWidth * inputHeight * inputDepth)));
+            var input = BuilderInstance<double>.Volume.Random(new Shape(inputWidth, inputHeight, inputDepth, bactchSize), 0.0,
+                Math.Sqrt(2.0 / (inputWidth * inputHeight * inputDepth)));
             var output = layer.DoForward(input, true);
 
             // Set output gradients to 1
@@ -24,33 +25,38 @@ namespace ConvNetSharp.Core.Tests
             layer.Backward(outputGradient);
             var computedGradients = layer.InputActivationGradients;
 
-            // Now let's approximate gradient using derivate definition
-            for (var d = 0; d < inputDepth; d++)
+            using (var result = BuilderInstance<double>.Volume.SameAs(output.Shape))
             {
-                for (var y = 0; y < inputHeight; y++)
+                // Now let's approximate gradient using derivate definition
+                for (var d = 0; d < inputDepth; d++)
                 {
-                    for (var x = 0; x < inputWidth; x++)
+                    for (var y = 0; y < inputHeight; y++)
                     {
-                        var oldValue = input.Get(x, y, d);
-
-                        input.Set(x, y, d, oldValue + epsilon);
-                        var output1 = (Volume<double>)layer.DoForward(input).Clone();
-                        input.Set(x, y, d, oldValue - epsilon);
-                        var output2 = (Volume<double>)layer.DoForward(input).Clone();
-
-                        input.Set(x, y, d, oldValue);
-
-                        output1 = output1 - output2;
-
-                        var grad = new double[output.Shape.TotalLength];
-                        for (var j = 0; j < output.Shape.TotalLength; j++)
+                        for (var x = 0; x < inputWidth; x++)
                         {
-                            grad[j] = output1.Get(j) / (2.0 * epsilon);
-                        }
+                            result.Clear();
 
-                        var gradient = grad.Sum(); // approximated gradient
-                        var actual = computedGradients.Get(x, y, d);
-                        Assert.AreEqual(gradient, actual, 1e-3); // compare layer gradient to the approximated gradient
+                            var oldValue = input.Get(x, y, d);
+
+                            input.Set(x, y, d, oldValue + epsilon);
+                            var output1 = layer.DoForward(input).Clone();
+                            input.Set(x, y, d, oldValue - epsilon);
+                            var output2 = layer.DoForward(input).Clone();
+
+                            input.Set(x, y, d, oldValue);
+
+                            output2.DoSubtractFrom(output1, result);
+
+                            var grad = new double[output.Shape.TotalLength];
+                            for (var j = 0; j < output.Shape.TotalLength; j++)
+                            {
+                                grad[j] = result.Get(j) / (2.0 * epsilon);
+                            }
+
+                            var gradient = grad.Sum(); // approximated gradient
+                            var actual = computedGradients.Get(x, y, d);
+                            Assert.AreEqual(gradient, actual, 1e-3); // compare layer gradient to the approximated gradient
+                        }
                     }
                 }
             }
@@ -61,7 +67,8 @@ namespace ConvNetSharp.Core.Tests
             layer.Init(inputWidth, inputHeight, inputDepth);
 
             // Forward pass
-            var input = BuilderInstance<double>.Volume.From(new double[inputWidth * inputHeight * inputDepth * bacthSize].Populate(1.0), new Shape(inputWidth, inputHeight, inputDepth, bacthSize));
+            var input = BuilderInstance<double>.Volume.From(new double[inputWidth * inputHeight * inputDepth * bacthSize].Populate(1.0),
+                new Shape(inputWidth, inputHeight, inputDepth, bacthSize));
             var output = layer.DoForward(input);
 
             // Set output gradients to 1
@@ -84,21 +91,24 @@ namespace ConvNetSharp.Core.Tests
 
                     var oldValue = vol.Get(i);
                     vol.Set(i, oldValue + epsilon);
-                    var output1 = (Volume<double>)layer.DoForward(input).Clone();
+                    var output1 = layer.DoForward(input).Clone();
                     vol.Set(i, oldValue - epsilon);
-                    var output2 = (Volume<double>)layer.DoForward(input).Clone();
+                    var output2 = layer.DoForward(input).Clone();
                     vol.Set(i, oldValue);
 
-                    output1 = output1 - output2;
-
-                    var grad = new double[output.Shape.TotalLength];
-                    for (var j = 0; j < output.Shape.TotalLength; j++)
+                    using (var result = BuilderInstance<double>.Volume.SameAs(output1.Shape))
                     {
-                        grad[j] = output1.Get(j) / (2.0 * epsilon);
-                    }
+                        output2.DoSubtractFrom(output1, result);
 
-                    var gradient = grad.Sum(); // approximated gradient
-                    Assert.AreEqual(gradient, gra.Get(i), 1e-3); // compare layer gradient to the approximated gradient
+                        var grad = new double[output.Shape.TotalLength];
+                        for (var j = 0; j < output.Shape.TotalLength; j++)
+                        {
+                            grad[j] = result.Get(j) / (2.0 * epsilon);
+                        }
+
+                        var gradient = grad.Sum(); // approximated gradient
+                        Assert.AreEqual(gradient, gra.Get(i), 1e-3); // compare layer gradient to the approximated gradient}
+                    }
                 }
             }
         }
