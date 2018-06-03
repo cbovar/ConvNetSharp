f87d1981febd1e42a39f7c8d973eb09e3f8f9b26
cbovar <cedric.bovar@gmail.com>
2018-06-03 10:55:51 +0800

    Allow broadcast

--- src/ConvNetSharp.Volume.GPU/Double/Kernels/Power.cu
+++ src/ConvNetSharp.Volume.GPU/Double/Kernels/Power.cu
@@ -1,6 +1,12 @@
 ﻿extern "C" {
-    __global__ void Run(int n, double* __restrict left, double* __restrict right, double* __restrict output) {
+    __global__ void Run(int n, double* __restrict left, double* __restrict right, double* __restrict output, int rightIsScalar) {
 		int i = blockIdx.x*blockDim.x + threadIdx.x;
-		if (i < n) output[i] = pow(left[i], right[i]);
+        if (i < n) {
+            if (rightIsScalar == 1){ 
+                output[i] = pow(left[i], right[0]);
+            } else {
+		        output[i] = pow(left[i], right[i]);
+            }
+        }
 	}
 }
