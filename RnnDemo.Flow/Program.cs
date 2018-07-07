7b466b6a6b4e356f560e8c3694e2d14e5c9713bf
cbovar <cedric.bovar@gmail.com>
2018-07-07 13:48:22 +0800

    Rnn demo

---
+++ RnnDemo.Flow/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace RnnDemo.Flow
+{
+    internal class Program
+    {
+        /// <summary>
+        /// This demo is has some issues:
+        /// - Network never manages to get every word right (not matter the size of hidden state)
+        /// - When starting a new epoch, loss increases. This is probably due to discontinuity in the state.
+        /// - Text generation is not impressive at all. Some real words are sometimes genereated but it's mostly garbage.
+        /// 
+        /// TODO: use LSTM
+        /// </summary>
+        private static void Main()
+        {
+            // Load Shakerpeare data
+            //var book = new StringBuilder();
+            //foreach (var file in Directory.EnumerateFiles(@"./shakespeare/", "*.txt"))
+            //{
+            //    using (var sr = new StreamReader(new FileStream(file, FileMode.Open, FileAccess.Read)))
+            //    {
+            //        book.Append(sr.ReadToEnd());
+            //    }
+            //}
+
+            //var textData = new TextData(book.ToString());
+            var textData = new TextData(File.ReadAllText("Simple.txt"));
+
+            // Start learning
+            var rnnDemo = new RnnDemo(textData, 3, 100, 300, 0.01f);
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine(Environment.NewLine + "---- Rnn demo ----");
+                Console.WriteLine("0) Learning");
+                Console.WriteLine("1) Text generation");
+                Console.WriteLine("2) Exit");
+
+                var c = Console.ReadKey();
+                switch (c.KeyChar)
+                {
+                    case '2':
+                        return;
+                    case '0':
+                        rnnDemo.Learn();
+                        break;
+                    case '1':
+                        rnnDemo.GenerateText();
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ RnnDemo.Flow/TextData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace RnnDemo.Flow
+{
+    internal class TextData
+    {
+        public TextData(string rawData)
+        {
+            this.RawData = rawData;
+            this.Vocabulary = rawData.Select(c => c).Distinct().OrderBy(c => c).ToList();
+        }
+
+        public string RawData { get; }
+
+        public List<char> Vocabulary { get; }
+    }
+}
