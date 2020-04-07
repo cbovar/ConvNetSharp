9f4f9717a8967b1123edbb6130da3082fb0c60e6
idow09 <idow09@gmail.com>
2020-04-07 12:21:27 +0300

    Added ReLUs, MaxPools and Conv2d

--- src/ConvNetSharp.Flow.Tests/Data/Onnx/python/models.py
+++ src/ConvNetSharp.Flow.Tests/Data/Onnx/python/models.py
@@ -5,10 +5,22 @@ from torch import nn
 class ConvModel(Module):
     def __init__(self):
         super(ConvModel, self).__init__()
-        self.conv = nn.Conv2d(1, 10, 28)
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.relu1 = nn.ReLU()
+        self.pool1 = nn.MaxPool2d(2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.relu2 = nn.ReLU()
+        self.pool2 = nn.MaxPool2d(2)
+        self.conv_reshape = nn.Conv2d(16, 10, 4)
 
     def forward(self, x):
-        y = self.conv(x)
+        y = self.conv1(x)
+        y = self.relu1(y)
+        y = self.pool1(y)
+        y = self.conv2(y)
+        y = self.relu2(y)
+        y = self.pool2(y)
+        y = self.conv_reshape(y)
         return y
 
 
